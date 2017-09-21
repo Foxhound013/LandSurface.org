2dd99c7243f5c4ad3f14d50789aab5cdbdc26858
W. Logan Downing <downingstech@gmail.com>
2017-09-21 18:45:58 -0500

    Convert side nav into full links
    Made the div chunks wrapped in a tags for the side nav container.

--- resources/includes/side-nav-container-items.php
+++ resources/includes/side-nav-container-items.php
@@ -1,53 +1,53 @@
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-ios-paper-outline icon-big"></i>
-      <h3>News &amp; Highlights</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-ios-paper-outline icon-big"></i>
+        <h3>News &amp; Highlights</h3>
+    </div>
   </div>
-</div>
+</a>
 
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-ios-book-outline icon-big"></i>
-      <h3>Publications</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-ios-book-outline icon-big"></i>
+        <h3>Publications</h3>
+    </div>
   </div>
-</div>
+</a>
 
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-ios-flask-outline icon-big"></i>
-      <h3>Research Areas</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-ios-flask-outline icon-big"></i>
+        <h3>Research Areas</h3>
+    </div>
   </div>
-</div>
+</a>
 
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-ios-people-outline icon-big"></i>
-      <h3>Team</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-ios-people-outline icon-big"></i>
+        <h3>Team</h3>
+    </div>
   </div>
-</div>
+</a>
 
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-images icon-big"></i>
-      <h3>Images</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-images icon-big"></i>
+        <h3>Images</h3>
+    </div>
   </div>
-</div>
+</a>
 
-<div class="row">
-  <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
-    <a href="#">
-      <i class="ion-compose icon-big"></i>
-      <h3>Courses</h3>
-    </a>
+<a href="#">
+  <div class="row">
+    <div class="col span-1-of-4 nav-box-with-icons secondary-nav-box-with-icons">
+        <i class="ion-compose icon-big"></i>
+        <h3>Courses</h3>
+    </div>
   </div>
-</div>
+</a>
