6dfe7eb682e33b7252950365c831c570d7a13352
W. Logan Downing <downingstech@gmail.com>
2017-09-21 19:12:28 -0500

    Added Jquery to header
    Included jQuery to project in order to detect what buttons are selected for the purpose of loading the proper text files into the content-template.

--- resources/includes/head.php
+++ resources/includes/head.php
@@ -6,4 +6,5 @@
   <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
   <link rel="stylesheet" type="text/css" href="resources/css/style.css">
   <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
